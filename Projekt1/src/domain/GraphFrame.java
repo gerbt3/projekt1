ae16010b91a0547f0a23f46dc156bbaff7b2f30f
Daria Schumacher <daria.schumacher@bluemail.ch>
2015-04-07 12:08:11 +0200

    Added a ActionListener to the new button.

--- Projekt1/src/domain/GraphFrame.java
+++ Projekt1/src/domain/GraphFrame.java
@@ -1,9 +1,12 @@
 package domain;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
@@ -37,6 +40,27 @@ public class GraphFrame extends JFrame {
 		JButton newButton = new JButton("New");
 		JButton saveButton = new JButton("Save");
 		JButton openButton = new JButton("Open");
+		
+		newButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent event) {
+		    	
+		    	Object[] options = {"Undirected graph", "Directed graph"};
+		    	int choice = JOptionPane.showOptionDialog(null,
+		    		    "Choose a graph option:",
+		    		    "Graph options",
+		    		    JOptionPane.YES_NO_CANCEL_OPTION,
+		    		    JOptionPane.QUESTION_MESSAGE,
+		    		    null,
+		    		    options,
+		    		    options[1]);
+		    	
+		        	if (choice == 0) {
+		         	   //Undirected graph
+		         	} else {
+		         	   //Directed graph
+		         	}
+		    	}
+		 });
 		
 		menuPanel.add(newButton);
 		menuPanel.add(saveButton);
